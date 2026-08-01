61c43d4b8e89ceb812a08ef7549855aebde90e72
natata7 <natata7@gmail.com>
2026-08-01 17:39:04 +0300

    refactor log filtering and counting functions for improved clarity and performance

--- 03_task.py
+++ 03_task.py
@@ -1,6 +1,6 @@
 def parse_log_line(line: str) -> dict:
     date, time, level, *message = line.split()
-    
+
     return {
         "date": date,
         "time": time,
@@ -12,12 +12,12 @@ def load_logs(file_path: str) -> list:
     with open(file_path, 'r', encoding='utf-8') as file:
         return [parse_log_line(line) for line in file.readlines()]
 
-def filter_logs_by_level(logs: list, level: str) -> list:
-    filtered_logs = []
-    for log in logs:
-        if log["level"] == level.upper():
-            filtered_logs.append(log)
-    return filtered_logs
+def filter_logs_by_level(logs: list, level: str | None) -> list:
+
+    if level is None:
+        return logs
+    
+    return list(filter(lambda log: log["level"] == level.upper(), logs))
 
 def count_logs_by_level(logs: list) -> dict:
     levels = ["DEBUG", "INFO", "WARNING", "ERROR"]
@@ -25,29 +25,24 @@ def count_logs_by_level(logs: list) -> dict:
     counts = {level: 0 for level in levels}
 
     for log in logs:
-        if log["level"] == levels[0]:
-            counts["DEBUG"] += 1
-        elif log["level"] == levels[1]:
-            counts["INFO"] += 1
-        elif log["level"] == levels[2]:
-            counts["WARNING"] += 1
-        elif log["level"] == levels[3]:
-            counts["ERROR"] += 1
+        counts[log["level"]] += 1
     return counts
 
 def display_log_counts(counts: dict):
     print("Рівень логування | Кількість")  
+    print("-----------------|----------")
     for level, count in counts.items():
         print(f"{level:<16} | {count}")
 
-def main(path, level):
+def main(path, level = None):
     logs = load_logs(path)
     filtered_logs = filter_logs_by_level(logs, level)
     counts = count_logs_by_level(logs)
     display_log_counts(counts)
 
-    print(f"\nДеталі логів для рівня '{level}':")
-    for log in filtered_logs:  
-        print(f"{log['date']} {log['time']} - {log['message']}")
+    if level:
+        print(f"\nДеталі логів для рівня '{level}':")
+        for log in filtered_logs:  
+            print(f"{log['date']} {log['time']} - {log['message']}")
 
-main("assets/log-file.txt", "ERROR")
+main("assets/log-file.txt")
